7634d15bd0c7709327ac70860b9d9a0e4eb1dde5
Aliaksandr Melnichenka <togpe22@gmail.com>
2024-09-08 01:01:05 -0400

    Added 2.5.38

--- ToDo.md
+++ ToDo.md
@@ -1,3 +1,5 @@
 ## Things to do
 - Make section with tips, and common tricks for the olympiad preparation
 - Make .MD to solutions converter using python
+- Fix a bug with hover effect on attached container on first run
+- Adjust all russian solutions to the English-articles style
--- src/path.py
+++ src/path.py
@@ -1,15 +1,15 @@
 import os
 
 # Define the range of folders to create
-start_year = 19
-end_year = 19
+start_year = 38
+end_year = 38
 
 # Define the folder structure
 base_path = '.'  # You can change this to your desired base path  
 current_folder = os.getcwd().replace("src", "en\\")
 for year in range(start_year, end_year + 1):
 
-    folder_name = f"{current_folder}1.2.{year}"
+    folder_name = f"{current_folder}2.5.{year}"
     folder_path = os.path.join(base_path, folder_name)
     
     # Create the folder if it doesn't exist
