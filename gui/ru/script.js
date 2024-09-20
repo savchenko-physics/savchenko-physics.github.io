36c736fbd367f85b76bd6418ae8e789cb2972814
Aliaksandr Melnichenka <togpe22@gmail.com>
2024-09-20 18:43:23 -0400

    Addded Russian GUI & Updated 3.1.1-ru

---
+++ gui/ru/script.js
@@ -0,0 +1,191 @@
+const areas = ['area1', 'area2', 'area3', 'area4'];
+
+areas.forEach(areaId => {
+    document.getElementById(areaId).addEventListener('input', function() {
+        const latexCode = this.value;
+        const outputElement = document.getElementById('output');
+        
+        outputElement.innerHTML = `
+        ${generateBasement()}<br><br><br>`;
+
+        MathJax.typesetPromise([outputElement]).catch((err) => console.log(err.message));
+    });
+});
+
+
+document.getElementById('viewRawCode').addEventListener('click', function() {
+    const htmlContent = generateHtmlContent()
+    var newTab = window.open('', '_blank');
+    
+    newTab.document.open();
+    newTab.document.write(htmlContent);
+    newTab.document.close();
+});
+
+function generateHtmlContent() {
+    const area1 = document.getElementById('area1').value;
+    const area2 = document.getElementById('area2').value;
+    const area3 = document.getElementById('area3').value;
+    const area4 = document.getElementById('area4').value;
+
+    const htmlContent = `<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="content-language" content="ru">
+    <meta name="keywords" content="решение, савченко, оливер, яковлевич, о.я., сборник задач по физике, решебник">
+    <meta name="description" content="${removeImageSection(area2)}">
+    <meta name="author" content="Aliaksandr Melnichenka">
+    <meta name="date" content="2023-10" scheme="YYYY-MM">
+    <meta property="og:title" content="${removeImageSection(area2)}">
+    <meta property="og:image" content="img/logo.png">
+    <meta property="og:description" content="${removeImageSection(area2)}">
+    <meta name="yandex-verification" content="6cfda41f74038368">
+    <title>${removeImageSection(area2)}</title>
+    <link rel="stylesheet" href="https://savchenkosolutions.com/css/css-latex/style.css">
+    <link rel="icon" href="https://savchenkosolutions.com/img/logo.png" type="image/png">
+    <script src="https://savchenkosolutions.com/js/jquery-1.10.1.min.js"></script>
+    <script async src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.7/MathJax.js?config=TeX-MML-AM_CHTML"></script>
+    <script type="text/x-mathjax-config">
+        MathJax.Hub.Config({
+            extensions: ['tex2jax.js'],
+            jax: ['input/TeX', 'output/HTML-CSS'],
+            tex2jax: {
+                inlineMath: [['$', '$'], ['$', '$']],
+                processEscapes: true,
+                processClass: 'tex2jax',
+                ignoreClass: 'html'
+            },
+            showProcessingMessages: false,
+            messageStyle: 'none'
+        });
+    </script>
+</head>
+  <body style="">
+      <header style="text-align:center;">
+    <h2>Решение задач из Савченко О.Я.</h2>
+    <p class="author">
+      Aliaksandr Melnichenka <br/>
+      October 2023
+    </p>
+  </header>
+
+        <h3 id="back-link"><a href="../../#${getFirstTwoSegments(document.getElementById('area1').value)}">$\\leftarrow$Назад</a></h3>
+    ${generateBasement()}
+
+    <footer class="row container">
+      <br>
+        <p>
+            <small> © <strong>Savchenko Solutions</strong>, 2023-2024 <br></small>
+        </p>
+        <p>
+            <small>Все права принадлежат авторам. <br> Коммерческое использование материалов — с письменного разрешения авторов. <br> alex@savchenkosolutions.com <br></small>
+        </p>
+    </footer>
+  </body>
+
+</html>`;
+
+    return ('<html><head><title>HTML Code</title>' +
+        '<style>body { background-color: black; color: white; }' +
+        '</style></head>' +
+        '<body><pre>' + 
+        htmlContent.replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/\\\)/g, "$").replace(/\\\(/g, '$').replace(/>/g, '&gt;') + 
+        '</pre></body></html>');
+}
+
+function getFirstTwoSegments(input) {
+  const segments = input.split('.');
+  
+  return segments.slice(0, 2).join('.');
+}
+
+function removeImageSection(inputString) {
+  const imageSectionRegex = /<\/p>[\s\S]*?<p>/g;
+  const cleanedString = inputString.replace(imageSectionRegex, '');
+
+  return cleanedString.replace(/\n/g, '').replace(/\$/g, '');
+}
+
+function generateBasement() {
+    const area1 = document.getElementById('area1').value;
+    const area2 = document.getElementById('area2').value;
+    const area3 = document.getElementById('area3').value;
+    const area4 = document.getElementById('area4').value;
+
+    const htmlContent = `
+        <h3> Условие </h3>
+        <p>
+            $${area1}.$ ${area2}
+        </p>
+
+        <h3>Решение</h3>
+        <p>
+            ${area3}
+        </p>
+
+        <h4>Ответ</h4>
+        <p>
+            ${area4}
+        </p>
+`;
+
+    return (htmlContent);
+}
+
+function handleDrop(event) {
+    event.preventDefault();
+
+    const textarea = event.target;
+    const dropPosition = textarea.selectionStart;
+    const droppedData = event.dataTransfer.getData('text/plain');
+
+    // Check if a URL is dropped
+    if (droppedData.startsWith('http') && droppedData.match(/\.(jpeg|jpg|png|gif)$/i)) {
+        const imageUrl = droppedData;
+
+        textarea.value =
+        `${textarea.value.substring(0, dropPosition)}
+</p>
+<center>
+  <figure>
+    <img src="${imageUrl}"
+      loading="lazy"  width="230" />
+    <figcaption>
+      К задаче $${document.getElementById('area1').value}$
+    </figcaption>
+  </figure>
+</center>
+<p>${textarea.value.substring(dropPosition)}`;
+        
+    } else if (event.dataTransfer.files.length > 0) {
+        // Handle local files
+        const file = event.dataTransfer.files[0];
+        const filePath = file.path || file.name;
+
+        textarea.value =
+        `${textarea.value.substring(0, dropPosition)}
+</p>
+<center>
+  <figure>
+    <img src="${filePath}"
+      loading="lazy"  width="230" />
+    <figcaption>
+      К задаче $${document.getElementById('area1').value}$
+    </figcaption>
+  </figure>
+</center>
+<p>${textarea.value.substring(dropPosition)}`;
+    }
+}
+function handleDragOver(event) {
+    event.preventDefault(); // Prevent default behavior to allow drop
+}
+
+['area2', 'area3'].forEach(id => {
+    const textarea = document.getElementById(id);
+    textarea.addEventListener('dragover', handleDragOver);
+    textarea.addEventListener('drop', handleDrop);
+});
